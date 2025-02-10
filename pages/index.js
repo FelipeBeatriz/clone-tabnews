fb56813704b94dd9586c2141c58f890ceb0d7fa9
Felipe Beatriz <felipe.beatriz199@gmail.com>
2025-02-10 21:59:01 +0000

    teste do gato

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 function Home() {
-    return <h1>Mensagem diferente!</h1>
+    return (
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <h1>u i i a i u u i i a</h1>
+            <img 
+                src="https://media.tenor.com/blwK0rdIId8AAAAi/cat-oiiaoiia-cat.gif" 
+                alt="Cat waving"
+                style={{ width: "200px", height: "auto" }}
+            />
+        </div>
+    );
 }
 
 export default Home;
-// ls -la
