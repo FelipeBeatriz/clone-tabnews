036d254739d2f54263f9ea572d3471817b2fa9b3
Felipe Beatriz <felipe.beatriz199@gmail.com>
2025-02-10 22:03:24 +0000

    volta pro texto original

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 function Home() {
-    return (
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-            <h1>u i i a i u u i i a</h1>
-            <img 
-                src="https://media.tenor.com/blwK0rdIId8AAAAi/cat-oiiaoiia-cat.gif" 
-                alt="Cat waving"
-                style={{ width: "200px", height: "auto" }}
-            />
-        </div>
-    );
+    return <h1>Mensagem nova!</h1>
 }
 
 export default Home;
+// ls -la
